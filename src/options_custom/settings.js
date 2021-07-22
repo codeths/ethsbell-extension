c488e2939e4ae569ad449e13e333459fc9ef4999
Albert Portnoy <albert@asportnoy.com>
2021-07-22 11:12:24 -0500

    Added code for option saving and loading

--- src/options_custom/settings.js
+++ src/options_custom/settings.js
@@ -1,60 +1,43 @@
-window.addEvent("domready", function () {
-    // Option 1: Use the manifest:
-    new FancySettings.initWithManifest(function (settings) {
-        settings.manifest.myButton.addEvent("action", function () {
-            alert("You clicked me!");
+const form = document.getElementById('option-form');
+const enabledInput = document.getElementById('notifs-enabled');
+const offsetInput = document.getElementById('notifs-offset');
+
+(async () => {
+    const settings = await getNotificationSettings();
+    enabledInput.checked = settings.enabled || false;
+    offsetInput.value = settings.offset ?? 3;
+})();
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    await setNotificationSettings({
+        enabled: enabledInput.checked,
+        offset: offsetInput.valueAsNumber || null
+    });
+
+    chrome.extension.sendRequest({
+        message: "reload"
+    });
+
+    alert('Saved.');
+    return false;
+});
+
+
+async function getNotificationSettings() {
+    return new Promise(async (resolve, reject) => {
+        chrome.storage.sync.get(['enabled', 'offset'], (data) => {
+            return resolve(data);
         });
     });
-    
-    // Option 2: Do everything manually:
-    /*
-    var settings = new FancySettings("My Extension", "icon.png");
-    
-    var username = settings.create({
-        "tab": i18n.get("information"),
-        "group": i18n.get("login"),
-        "name": "username",
-        "type": "text",
-        "label": i18n.get("username"),
-        "text": i18n.get("x-characters")
+}
+
+async function setNotificationSettings(settings) {
+    const { enabled, offset } = settings;
+    return new Promise(async (resolve, reject) => {
+        chrome.storage.sync.set({ enabled, offset }, () => {
+            return resolve();
+        });
     });
-    
-    var password = settings.create({
-        "tab": i18n.get("information"),
-        "group": i18n.get("login"),
-        "name": "password",
-        "type": "text",
-        "label": i18n.get("password"),
-        "text": i18n.get("x-characters-pw"),
-        "masked": true
-    });
-    
-    var myDescription = settings.create({
-        "tab": i18n.get("information"),
-        "group": i18n.get("login"),
-        "name": "myDescription",
-        "type": "description",
-        "text": i18n.get("description")
-    });
-    
-    var myButton = settings.create({
-        "tab": "Information",
-        "group": "Logout",
-        "name": "myButton",
-        "type": "button",
-        "label": "Disconnect:",
-        "text": "Logout"
-    });
-    
-    // ...
-    
-    myButton.addEvent("action", function () {
-        alert("You clicked me!");
-    });
-    
-    settings.align([
-        username,
-        password
-    ]);
-    */
-});
+}
