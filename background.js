d49d41b06f567ee3701043dba7d468e99df09db3
mswider <matthew.swider@outlook.com>
2023-02-24 19:38:48 -0600

    feat: add instance support to browser action

--- background.js
+++ background.js
@@ -5,7 +5,7 @@ async function setTimers(force = false) {
 		return;
 	}
 
-	const apidata = await get('/api/v1/today');
+	const apidata = await get('https://ethsbell.app', '/api/v1/today');
 	if (!apidata) {
 		return;
 	}
@@ -81,7 +81,7 @@ function findApplicablePeriodEnds(periods, timestamp, offset = null) {
 
 // Run notification
 async function runNotification(timestamp = Date.now(), mockTime = null) {
-	const today = await get('/api/v1/today');
+	const today = await get('https://ethsbell.app' ,'/api/v1/today');
 	if (!today || !today.periods || !today.periods[0]) {
 		return;
 	}
