dd8ab1ffa55ce3a436f2c99edddb4ec43edf099b
mswider <matthew.swider@outlook.com>
2025-07-28 15:36:32 -0500

    fix: correct async js usage, style changes

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,45 +1,29 @@
 import { getSyncData, getInstanceDomain } from './static/utils.js';
 
-chrome.runtime.onMessageExternal.addListener(async (message, sender) => {
-	if (message && message.message === 'schedule' && message.data) {
-		try {
-			const data = JSON.parse(message.data);
-			if (!data) {
-				return;
-			}
-			chrome.storage.local.set({ config: data });
-		} catch {}
-	}
-});
-
 async function getNotificationOffset() {
-	let enabled = await getSyncData('enabled');
-	let offset = await getSyncData('offset');
-	if (!enabled) {
-		return null;
-	}
+	const enabled = await getSyncData('enabled');
+	const offset = await getSyncData('offset');
+	if (!enabled) return null;
 	return offset ?? null;
 }
 
 // Set timers for the period end times
 async function setTimers(force = false) {
-	let instance = await getInstanceDomain();
+	const instance = await getInstanceDomain();
 	const offset = await getNotificationOffset();
-	if (offset === null) {
-		return;
-	}
-	let apidata = await fetch(`${instance}/api/v1/today`);
-	apidata = await apidata.json();
-	if (!apidata) {
-		return;
-	}
+	console.log('setTimers()');
+	if (offset === null) return;
+
+	const apidata = await (await fetch(`${instance}/api/v1/today`)).json();
+	console.log({ apidata });
+	if (!apidata) return;
 
 	await clearNotificationAlarms();
-	let endTimes = apidata.periods;
-	endTimes = endTimes
+
+	let endTimes = apidata.periods
 		.map((period) => (period.end_timestamp - offset * 60) * 1000)
 		.filter((timestamp, pos, array) => timestamp > Date.now() && array.indexOf(timestamp) === pos);
-	console.log(endTimes);
+	console.log({ endTimes });
 	endTimes.forEach((timestamp) => {
 		console.log('creating alarm... ' + timestamp);
 		chrome.alarms.create(`Notify-${timestamp}`, {
@@ -50,18 +34,18 @@ async function setTimers(force = false) {
 
 // Clear all notification timers
 async function clearNotificationAlarms() {
-	await chrome.alarms.getAll(async (alarms) => {
-		for (const alarm of alarms) {
-			if (alarm.name.startsWith('Notify')) {
-				clearAlarm(alarm.name);
-			}
+	console.log('clearNotificationAlarms()');
+	const alarms = await chrome.alarms.getAll();
+	for (const alarm of alarms) {
+		if (alarm.name.startsWith('Notify')) {
+			await clearAlarm(alarm.name);
 		}
-		return;
-	});
+	}
 }
 
-// Clear alarm by name (promisified)
+// Clear alarm by name
 async function clearAlarm(name) {
+	console.log('clearAlarm() name:', name);
 	await chrome.alarms.clear(name);
 }
 
@@ -69,7 +53,7 @@ async function clearAlarm(name) {
 async function runManual() {
 	chrome.notifications.create('', {
 		type: 'basic',
-		iconUrl: '/icons/icon128.png',
+		iconUrl: '/src/icons/icon128.png',
 		title: 'ETHSBell Notification',
 		message: 'Notifications work!',
 	});
@@ -87,31 +71,27 @@ function findApplicablePeriodEnds(periods, timestamp, offset = null) {
 
 // Run notification
 async function runNotification(timestamp = Date.now()) {
-	let instance = await getInstanceDomain();
-	let today = await fetch(`${instance}/api/v1/today`);
-	today = await today.json();
+	console.log('runNotification()');
+	const instance = await getInstanceDomain();
+	const today = await (await fetch(`${instance}/api/v1/today`)).json();
+
 	if (!today || !today.periods || !today.periods[0]) {
 		return;
 	}
 
 	const offset = await getNotificationOffset();
-	if (offset === null) {
-		return;
-	}
+	if (offset === null) return;
 
 	const currentPeriods = findApplicablePeriodEnds(today.periods, timestamp, offset);
-	if (!currentPeriods || !currentPeriods[0]) {
-		return;
-	}
+	if (!currentPeriods || !currentPeriods[0]) return;
 
 	const periods = today.periods.filter((p) => p.end_timestamp === currentPeriods[0].end_timestamp);
 
 	const endTime = currentPeriods.end_timestamp * 1000;
 	const now = Date.now();
 	const timeLeftInMinutes = (endTime - now) / 1000 / 60 / 60;
-	if (timeLeftInMinutes < -1) {
-		return;
-	}
+	if (timeLeftInMinutes < -1) return;
+
 	console.log('creating...');
 	chrome.notifications.create('', {
 		type: 'basic',
@@ -132,22 +112,6 @@ function getNotificationMessage(period, name, now) {
 		message: `${name} ends at ${endTimeString}.`,
 	};
 }
-
-// When alarm goes off
-
-chrome.alarms.onAlarm.addListener((alarm) => {
-	console.log(alarm);
-	// Refresh data
-	if (alarm.name === 'PullData') {
-		setTimers();
-		setPullDataAlarm();
-	}
-
-	// Period notifier
-	if (alarm.name.startsWith('Notify-')) {
-		runNotification(alarm.scheduledTime);
-	}
-});
 
 // Set timer for next day
 async function setPullDataAlarm() {
@@ -184,29 +148,63 @@ function plural_suffix(number, string) {
 	return `${string}${number === 1 ? '' : 's'}`;
 }
 
-chrome.runtime.onStartup.addListener(() => {
-	setTimers();
-	setPullDataAlarm();
-});
-chrome.runtime.onInstalled.addListener(() => {
-	setTimers();
-	setPullDataAlarm();
-});
+async function startup() {
+	await setTimers();
+	await setPullDataAlarm();
+}
 
-chrome.runtime.onMessage.addListener((message) => {
-	if (message.message === 'reload') {
-		setTimers();
-		setPullDataAlarm();
-	}
-
-	if (message.message === 'test') {
-		runManual();
-	}
-
-	if (message.message === 'reload-force') {
-		setTimers(true);
+chrome.runtime.onMessageExternal.addListener(async (message) => {
+	if (message && message.message === 'schedule' && message.data) {
+		try {
+			const data = JSON.parse(message.data);
+			if (!data) return;
+			chrome.storage.local.set({ config: data });
+		} catch {}
 	}
 });
 
-setTimers();
-setPullDataAlarm();
+// When alarm goes off
+chrome.alarms.onAlarm.addListener(async (alarm) => {
+	console.log(alarm);
+	// Refresh data
+	if (alarm.name === 'PullData') {
+		await setTimers();
+		await setPullDataAlarm();
+	}
+
+	// Period notifier
+	if (alarm.name.startsWith('Notify-')) {
+		await runNotification(alarm.scheduledTime);
+	}
+});
+
+chrome.runtime.onMessage.addListener(async (message) => {
+	console.log('got message:', message);
+
+	switch (message.message) {
+		case 'reload':
+			await startup();
+			break;
+
+		case 'test':
+			await runManual();
+			break;
+
+		case 'reload-force':
+			await setTimers(true);
+			break;
+
+		default:
+			console.log('Unknown message type:', message.message);
+			break;
+	}
+});
+
+/**
+ * Runs whenever the service worker starts
+ * Ex: on browser launch, extension restarts, updates, messages, etc.
+ *
+ * Stuff like chrome.runtime.onStartup only runs when the browser launches,
+ * but in our case, we'd prefer to have more frequent refreshes.
+ */
+startup();
